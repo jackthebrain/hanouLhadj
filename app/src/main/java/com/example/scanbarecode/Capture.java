7aaefc7bef78bd6a622ea53d451c28bd143a45bd
jaackthebrain <mofree197@gmail.com>
2023-06-15 15:10:23 +0100

    bismilah

---
+++ app/src/main/java/com/example/scanbarecode/Capture.java
@@ -0,0 +1,7 @@
+package com.example.scanbarecode;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class Capture extends CaptureActivity {
+
+}
--- app/src/main/java/com/example/scanbarecode/MainActivity.java
+++ app/src/main/java/com/example/scanbarecode/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.scanbarecode;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Button scan;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scan = findViewById(R.id.scan);
+
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ScanCode();
+            }
+        });
     }
+    private void ScanCode (){
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(Capture.class);
+        barLauncher.launch(options);
+
+    }
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(),result ->{
+        if (result.getContents() != null) {
+            Toast.makeText(this, "the code is " + result.getContents(), Toast.LENGTH_SHORT).show();
+        }
+    });
 }
